4644fd304ebdfe3e692227829e10d4c4f5cc3c69
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-13 17:39:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 let num1 = 2;
 let num2 = 31;
 
-let num3 = 31;
-let num4 = 31;
+let num3 = 24;
+let num4 = 5;
 
 const multiply = num1*num2;
 
 const radom = Math.floor(Math.random()*100)+1;
 
 const mod = num3%num4;
+
